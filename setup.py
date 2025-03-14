01cdba6c8665a88bff8a8e8b131c1c21fd9f9c74
Koaha <ldvankhoa@gmail.com>
2025-03-14 15:19:56 +0700

    fix ci cd installation version

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ class PostInstallCommand(install):
 
 setup(
     name='vitalDSP', 
-    version='0.1.1rc27',  
+    version='0.1.2',  
     author='van-koha',  
     author_email='vital.data@oucru.org', 
     description='A comprehensive toolkit for Digital Signal Processing in healthcare applications.',
