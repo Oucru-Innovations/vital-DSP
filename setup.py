7dacc71bf20d427c3b30fc255fa8e38bd05f2a96
Koaha <ldvankhoa@gmail.com>
2025-09-30 12:44:14 +0700

    version 0.1.5

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ class PostInstallCommand(install):
 
 setup(
     name='vitalDSP', 
-    version='0.1.4',  
+    version='0.1.5',  
     author='van-koha',  
     author_email='vital.data@oucru.org', 
     description='A comprehensive toolkit for Digital Signal Processing in healthcare applications.',
